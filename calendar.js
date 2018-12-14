3d846295ae429ab04b2c039aa2ad2108d5b96406
Tian Peng <ianpeng97@hotmail.com>
2018-12-14 17:02:11 -0500

    Update calendar.js

--- calendar.js
+++ calendar.js
@@ -3,6 +3,16 @@
 document.addEventListener('DOMContentLoaded', function() {
 });
 
+var check = function (id, selection) {
+    if (+id.split("-").join("") >= 0) {
+        if (selection.innerHTML == '') {
+            selection.innerHTML = '&#10003';
+        } else {
+            selection.innerHTML = '';
+        }
+    }
+};
+
 function onClick(event) {
     /* La variable t contient l'élément HTML sur lequel le clic a été
        fait. Notez qu'il ne s'agit pas forcément d'une case <td> du
@@ -14,26 +24,25 @@ function onClick(event) {
     var selection = document.getElementById(id);
 
     // verifie si le id est un nb (si oui, c'est une case du tab)
-    if (+id.split("-").join("") >= 0) {
-        if (selection.innerHTML == '') {
-            selection.innerHTML = '&#10003';
-        } else {
-            selection.innerHTML = '';
-        }
-    }
+    check(id, selection);
 }
 
 function onMove(event) {
     var t = event.target;
     var id = t.id;
+    var selection = document.getElementById(id);
+    var buttonStatus= event.buttons;
+    if (buttonStatus == 1) {
+        check(id, selection);
+    }
 }
 
 var compacterDisponibilites = function() {
-    
+
     var cal = document.getElementById("calendrier");
     var nbHeures = cal.dataset.nbheures;
     var nbJours = cal.dataset.nbjours;
-    
+
     var resultat = [];
     var selection;
 
